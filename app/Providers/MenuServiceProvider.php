e538ea420d65c9bb462c5bfcc064039684079ab1
obeyda <obeydaSarakbi@outlook.com>
2023-10-11 23:17:20 +0300

    soft box

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class MenuServiceProvider extends ServiceProvider
@@ -26,7 +27,6 @@ class MenuServiceProvider extends ServiceProvider
     $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
     $verticalMenuData = json_decode($verticalMenuJson);
 
-    // Share all menuData to all the views
-    \View::share('menuData', [$verticalMenuData]);
+    View::share('menuData', $verticalMenuData);
   }
 }
